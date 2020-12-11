0977e3f66ff66fd65c7d963fbd204bbd2e555b3d
TNNiedner <nevesth@gmail.com>
2020-12-11 01:42:07 -0300

    Using mongoDB

---
+++ src/main/java/com/niedner/workshopmongo/services/UserService.java
@@ -0,0 +1,21 @@
+package com.niedner.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.niedner.workshopmongo.domain.User;
+import com.niedner.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll() {
+		return repo.findAll();
+	}
+
+}
