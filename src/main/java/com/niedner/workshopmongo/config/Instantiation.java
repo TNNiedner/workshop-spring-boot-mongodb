8bc4f1210ff7a9b05d2e0a41bb8f5eb927e05424
TNNiedner <nevesth@gmail.com>
2020-12-13 15:55:47 -0300

    AuthorDTO

--- src/main/java/com/niedner/workshopmongo/config/Instantiation.java
+++ src/main/java/com/niedner/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.niedner.workshopmongo.domain.Post;
 import com.niedner.workshopmongo.domain.User;
+import com.niedner.workshopmongo.dto.AuthorDTO;
 import com.niedner.workshopmongo.repository.PostRepository;
 import com.niedner.workshopmongo.repository.UserRepository;
 
@@ -35,11 +36,11 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei", maria);
-		
-		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei", new AuthorDTO(maria));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 
 	}
